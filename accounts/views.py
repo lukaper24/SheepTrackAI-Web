528d86fe2051ff5ea63e012db5e1012ab28d6b3e
Luka <luka.per2402@gmail.com>
2026-06-04 12:20:49 +0200

    Implement user registration and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registracija je uspješna. Dobrodošao!")
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "accounts/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "Uspješno si prijavljen.")
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "accounts/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Uspješno si odjavljen.")
+    return redirect("home")
